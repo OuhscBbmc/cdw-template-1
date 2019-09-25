bef0090eacd64b5936c864365b5407354f71aad6
Will Beasley <wibeasley@hotmail.com>
2019-09-24 19:18:57 -0500

    add `populate_config()`

--- utility/spawn.R
+++ utility/spawn.R
@@ -4,12 +4,19 @@ remotes::install_github("OuhscBbmc/pluripotent")
 
 # Discover repo name & parent directory.
 project_name          <- basename(normalizePath(".."))
+project_path          <- normalizePath("..")
 project_parent_path   <- normalizePath("..\\..")
+config_path           <- file.path(project_path, "config.yml")
 
 # Copy remote files to the current repo.
 pluripotent::start_cdw_skeleton_1(
   project_name          = project_name,
   destination_directory = project_parent_path
+)
+
+pluripotent::populate_config(
+  path_in       = config_path,
+  project_name  = project_name
 )
 
 # Afterwards, close RStudio and open the *.Rproj file in the root directory.
